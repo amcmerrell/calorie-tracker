b8b7bd5dfe9c1165f9630f3b6b40410316f41343
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-21 08:02:57 -0700

    add form fields and basic component template for edit-food.

---
+++ app/edit-food.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Food } from './food.model';
+
+@Component({
+  selector: 'edit-food',
+  template:`
+    <div *ngIf="childSelectedFood">
+      <div class="form-group">
+        <label>Edit name:</label>
+        <input class="form-control" [(ngModel)]="childSelectedFood.name">
+      </div>
+      <div class="form-group">
+        <label>Edit description:</label>
+        <input class="form-control" [(ngModel)]="childSelectedFood.description">
+      </div>
+      <div class="form-group">
+        <label>Edit calories:</label>
+        <input class="form-control" [(ngModel)]="childSelectedFood.calories">
+      </div>
+      <button type="button" class="btn btn-warning" (click)="doneClicked()">Done</button>
+    </div>
+  `
+})
+export class EditFoodComponent {
+  @Input() childSelectedFood: Food;
+  @Output() doneClickedSender = new EventEmitter();
+  doneClicked() {
+    this.doneClickedSender.emit();
+  }
+}

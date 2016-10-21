858d639a4efd572a9c0ea7f50d9d62f9bb8e6dd2
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-21 10:28:13 -0700

    update sample food data to include dates.

--- app/app.component.ts
+++ app/app.component.ts
@@ -23,9 +23,9 @@ import { Food } from './food.model';
 
 export class AppComponent {
   public masterFoodList: Food[] = [
-    new Food("Pizza", "Extra Cheese and pepperoni.", 600),
-    new Food("Banana", "Post-lunch snack", 90),
-    new Food("Soda", "Post-lunch snack", 150)
+    new Food("Pizza", "Extra Cheese and pepperoni.", 600, new Date()),
+    new Food("Banana", "Post-lunch snack", 90, new Date()),
+    new Food("Soda", "Post-lunch snack", 150, new Date())
   ];
 
   public selectedFood = null;
